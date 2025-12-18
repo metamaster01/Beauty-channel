99e3235d52a29ea4d6c336491d67f7de622196b9
Akash Kumar <akash.tech2511@gmail.com>
2025-12-18 23:21:27 +0530

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,17 @@
 import AboutUs from "@/components/aboutus";
 import Hero from "@/components/hero";
-<<<<<<< Updated upstream
 import WhyChoose from "@/components/whychoose";
-=======
-import Offer from "@/components/offer";
-import OurServices from "@/components/ourservices";
->>>>>>> Stashed changes
+
 import Image from "next/image";
 
 export default function Home() {
   return (
      <main className="min-h-screen bg-white overflow-x-hidden">
       <Hero/>
-<<<<<<< Updated upstream
-      <WhyChoose/>
-=======
       <AboutUs/>
-      <OurServices/>
-      <Offer/>
->>>>>>> Stashed changes
+      <WhyChoose/>
+      
+      
       </main>
   );
 }
