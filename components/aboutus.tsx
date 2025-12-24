b003c3a58c2d59661a9d68599e2dfdf6f1aaeaee
Akash Kumar <akash.tech2511@gmail.com>
2025-12-24 23:10:23 +0530

    new updates

--- components/aboutus.tsx
+++ components/aboutus.tsx
@@ -26,7 +26,7 @@ const bottomBrands = [
 
 export default function AboutUs() {
   return (
-    <section className="w-full bg-[#F4F4F4] py-14">
+    <section className="w-full bg-[#0F0F0F] py-14">
       <div className="max-w-6xl mx-auto px-6">
 
         {/* ================= TOP ================= */}
@@ -38,18 +38,18 @@ export default function AboutUs() {
             viewport={{ once: true }}
             transition={{ duration: 0.6 }}
           >
-            <div className="flex items-center gap-2 text-sm text-[#B46A4C] font-medium mb-4">
-              <span className="w-2 h-2 rounded-full bg-[#B46A4C]" />
+            <div className="flex items-center gap-2 text-sm text-[#FFFFFF] font-medium mb-4">
+              <span className="w-2 h-2 rounded-full bg-[#B08D3C]" />
               About us
             </div>
 
-            <h2 className="text-[38px] leading-tight font-light text-black max-w-xl">
-              We help people look & feel their
+            <h2 className="text-[38px] leading-tight font-light text-[#FFFFFF] max-w-xl">
+              We help people look & feel their most beautiful
               <br />
-              <span className="font-medium">most beautiful</span>
+              {/* <span className="font-medium">most beautiful</span> */}
             </h2>
 
-            <p className="mt-5 text-black max-w-lg leading-relaxed">
+            <p className="mt-5 text-[#FFFFFF] max-w-lg leading-relaxed">
               Our salon believes confidence starts with self-care. With expert
               hair, skin, and makeup services, certified stylists, and premium
               products, we bring out your natural glow.
@@ -65,15 +65,15 @@ export default function AboutUs() {
             className="flex gap-10 items-start"
           >
             <div className="border-l border-gray-400 pl-6">
-              <h3 className="text-3xl font-semibold text-black">110+</h3>
-              <p className="text-black mt-1">Clients Styled</p>
-              <span className="text-sm italic text-black">All time</span>
+              <h3 className="text-3xl font-semibold text-[#FFFFFF]">110+</h3>
+              <p className="text-[#FFFFFF] mt-1">Clients Styled</p>
+              <span className="text-sm italic text-[#FFFFFF]">All time</span>
             </div>
 
             <div className="border-l border-gray-400 pl-6">
-              <h3 className="text-3xl font-semibold text-black">64</h3>
-              <p className="text-black mt-1">Makeovers Completed</p>
-              <span className="text-sm italic text-black">
+              <h3 className="text-3xl font-semibold text-[#FFFFFF]">64</h3>
+              <p className="text-[#FFFFFF] mt-1">Makeovers Completed</p>
+              <span className="text-sm italic text-[#FFFFFF]">
                 Bridal & Occasion
               </span>
             </div>
@@ -88,7 +88,7 @@ export default function AboutUs() {
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
             transition={{ duration: 0.6 }}
-            className="text-black max-w-xs text-2xl font-medium"
+            className="text-sm font-medium text-[#FFFFFF] bg-[#1D1D1D] rounded-2xl  px-6 py-5 flex  "
           >
             Trusted by top beauty & wellness brands
           </motion.p>
@@ -105,13 +105,13 @@ export default function AboutUs() {
                   viewport={{ once: true }}
                   transition={{ duration: 0.5, delay: i * 0.1 }}
                   whileHover={{ y: -6 }}
-                  className="bg-white rounded-2xl px-6 py-5 flex items-center justify-between shadow-sm hover:shadow-lg transition"
+                  className="bg-[#1D1D1D] rounded-2xl px-6 py-5 flex items-center justify-between shadow-sm hover:shadow-lg transition"
                 >
-                  <span className="text-sm font-medium text-black">
+                  <span className="text-sm font-medium text-[#FFFFFF]">
                     {item.name}
                   </span>
 
-                  <span className="w-10 h-10 rounded-full bg-[#C77B58] flex items-center justify-center text-white">
+                  <span className="w-10 h-10 rounded-full bg-[#B08D3C] flex items-center justify-center text-white">
                     <Icon size={18} />
                   </span>
                 </motion.div>
@@ -132,13 +132,13 @@ export default function AboutUs() {
                 viewport={{ once: true }}
                 transition={{ duration: 0.5, delay: i * 0.1 }}
                 whileHover={{ y: -6 }}
-                className="bg-white rounded-2xl px-6 py-5 flex items-center justify-between shadow-sm hover:shadow-lg transition"
+                className="bg-[#1D1D1D] rounded-2xl px-6 py-5 flex items-center justify-between shadow-sm hover:shadow-lg transition"
               >
-                <span className="text-sm font-medium text-black">
+                <span className="text-sm font-medium text-[#FFFFFF]">
                   {item.name}
                 </span>
 
-                <span className="w-10 h-10 rounded-full bg-[#C77B58] flex items-center justify-center text-white">
+                <span className="w-10 h-10 rounded-full bg-[#B08D3C] flex items-center justify-center text-[#FFFFFF]">
                   <Icon size={18} />
                 </span>
               </motion.div>
@@ -152,12 +152,12 @@ export default function AboutUs() {
             viewport={{ once: true }}
             transition={{ duration: 0.5, delay: 0.3 }}
             whileHover={{ y: -6 }}
-            className="bg-white rounded-2xl px-6 py-5 flex items-center justify-between border border-gray-400 hover:border-[#C77B58] shadow-sm hover:shadow-lg transition cursor-pointer"
+            className="bg-[#1D1D1D] rounded-2xl px-6 py-5 flex items-center justify-between border border-gray-400 "
           >
-            <span className="text-sm font-medium text-black">
+            <span className="text-sm font-medium text-[#FFFFFF]">
               Partner with us
             </span>
-            <span className="w-10 h-10 rounded-full bg-[#C77B58] flex items-center justify-center text-white">
+            <span className="w-10 h-10 rounded-full bg-[#B08D3C] flex items-center justify-center text-white">
               <ArrowRight size={16} />
             </span>
           </motion.div>
@@ -172,7 +172,7 @@ export default function AboutUs() {
         >
           <Link
             href="/about"
-            className="inline-flex items-center gap-2 mt-10 bg-[#C77B58] text-white px-6 py-3 rounded-full text-sm font-medium hover:bg-[#AF6A4A] transition"
+            className="inline-flex items-center gap-2 mt-10 bg-[#B08D3C] text-white px-6 py-3 rounded-full text-sm font-medium hover:bg-[#AF6A4A] transition"
           >
             More about us <ArrowRight size={16} />
           </Link>
