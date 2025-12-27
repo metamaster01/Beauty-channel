4c4c8905e04f4c5c45f0ffefe97a8d0de4346153
aavi4012-cmd <heyitzavinash@gmail.com>
2025-12-27 13:34:31 +0530

    page

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ import Gallery from "@/components/gallery";
 
 import AppointmentHero from "@/components/appoinment"; 
 import ContactFaqMock from "@/components/contactfaqmock";
-import BeautyMarquee from "@/components/beautymarquee";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import Advertisement from "@/components/advertisement";
-import Offer from "@/components/offer";
+// import Offer from "@/components/offer";
 import OurServices from "@/components/ourservices";
 import Image from "next/image";
+import Products from "@/components/product";
 
 export default function Home() {
   return (
@@ -21,14 +21,14 @@ export default function Home() {
       <Hero/>
       <AboutUs/>
       <OurServices/>
+      <Products/>
       <Advertisement/>
-      <Offer/>
+      {/* <Offer/> */}
       <WhyChoose/>   
       <Feedback/>
       <Gallery/>
       <AppointmentHero/>
       <ContactFaqMock/>
-    <BeautyMarquee/>
       <Footer/>
       
       </main>
